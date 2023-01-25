df843f7293a91fd991641d5926e3c418dbba3d59
lshin914 <97551104+lshin914@users.noreply.github.com>
2023-01-25 11:55:05 -0500

    second edit in new branch

--- new_file.java
+++ new_file.java
@@ -1,6 +1,6 @@
 public class new_file{
    public static void main(String[] args){
-      System.out.println("This is a new file and my second edit! I have now created an new branch.");
+      System.out.println("I have now created an new branch and this is my second edit.");
    }
 }
 
