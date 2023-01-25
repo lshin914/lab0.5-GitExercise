c12b76adec6997e541ad459eef64c73442dd50e8
lshin914 <97551104+lshin914@users.noreply.github.com>
2023-01-25 12:18:45 -0500

    changing file in branch

--- new_file.java
+++ new_file.java
@@ -1,6 +1,6 @@
 public class new_file{
    public static void main(String[] args){
-      System.out.println("Trying to create a conflict.");
+      System.out.println("Trying to fix a conflct.");
    }
 }
 
