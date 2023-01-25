18052a6349eea5fa1fe0ee517c636d02e5a7b872
lshin914 <97551104+lshin914@users.noreply.github.com>
2023-01-25 11:47:56 -0500

    my second edit

--- new_file.java
+++ new_file.java
@@ -1,6 +1,6 @@
 public class new_file{
    public static void main(String[] args){
-      System.out.println("This is a new file and my first edit!");
+      System.out.println("This is a new file and my second edit!");
    }
 }
 
