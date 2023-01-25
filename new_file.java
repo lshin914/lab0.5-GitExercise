c24719f29ad9eaf9a5c390a2545c9fe1c8e02cc3
lshin914 <97551104+lshin914@users.noreply.github.com>
2023-01-25 12:09:03 -0500

    second attempt at fixing conflict

--- new_file.java
+++ new_file.java
@@ -1,10 +1,6 @@
 public class new_file{
    public static void main(String[] args){
-<<<<<<< HEAD
-      System.out.println("Trying to fix confict.");
-=======
       System.out.println("Trying to fix conflict.");
->>>>>>> new_branch
    }
 }
 
